e2d0414b38bf56390088fdc55e486d5a7b66bd6e
Siddhi <siddhi.sritha.shetkar@gmail.com>
2026-06-16 16:11:19 +0530

    Initial commit

---
+++ Variables and Datatypes/first.js
@@ -0,0 +1,27 @@
+console.log("Hello, World!"); 
+name = "Alice";
+console.log("My name is " + name);
+age = 30;
+console.log(age);
+let totalPrice = 19.99;
+console.log("The total price is $" + totalPrice);  
+const PI = 3.14159;
+console.log("The value of PI is " + PI);
+
+{
+    let b = 20;
+    console.log("The value of b is " + b);
+}
+{
+    let b = 20;
+    console.log("The value of b is " + b);
+}
+
+const student = {
+    name: "John",
+    age: 20,
+    grade: "A"
+};
+
+console.log("Student Name: " + student.name);
+console.log("Student Age: " + student["age"]);
